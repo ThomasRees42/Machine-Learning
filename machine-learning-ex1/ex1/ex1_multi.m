6acb09811726842257f2c74145c9bcd0df8af4d8
Thomas Rees <TSR90909@gmail.com>
2020-04-28 12:34:51 +0100

    Completed optional components of task 1

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -82,7 +82,7 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
+alpha = 1;
 num_iters = 400;
 
 % Init Theta and Run Gradient Descent 
@@ -104,9 +104,9 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
-
-
+values = [1650 3];
+scaledValues = (values - mu) ./ sigma;
+price =  [1 scaledValues] * theta;
 % ============================================================
 
 fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
@@ -149,9 +149,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
-
-
+values = [1650 3];
+price = [1 values] * theta;
 % ============================================================
 
 fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
